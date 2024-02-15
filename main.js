fb08f8a947287f1bdafc0f96c41308c49dc73c0c
M2a1h1s <159919165+M2a1h1s@users.noreply.github.com>
2024-02-15 16:44:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+lancerJeu()
+ let inputEcriture = document.getById("inputEcriture")
+console.log("inputEcriture")
+
+let btnValiderMot = document.getById("btnValiderMot")
+console.log("btnValiderMot")
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log("zoneProposition")
+
+let zoneScore = document.querySelector(".zoneScore span")
+console.log("zoneScore")
+
+let inputBtnRadio = document.querySelectorAll(".zoneOptions input")
+console.log("inputBtnRadio")
